4972ead78d93c871477f47f2a185f8187a4d5075
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-01-15 21:55:37 -0700

    Update install-devils-extras.sh

--- install-devils-extras.sh
+++ install-devils-extras.sh
@@ -12,5 +12,6 @@ fi
 
 echo "Cloning Devils-Extra into $RPS_HOME"
 git clone https://github.com/Retro-Devils/Devils-Extra.git /home/pi/RetroPie-Setup/ext/Devils-Extra
+sudo cp /home/pi/RetroPie-Setup/ext/Devils-Extra/up-extras -f /usr/local/bin/
 echo "Running RetroPie-Setup"
 sudo ~/RetroPie-Setup/retropie_setup.sh
