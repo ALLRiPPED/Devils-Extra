208d86aeb1be4409e335aaa6b30ad9f85ffe43b2
ALLRiPPED <37816162+ALLRiPPED@users.noreply.github.com>
2022-02-06 22:59:54 -0600

    Fixing openbor

--- scriptmodules/emulators/openbor-extra.sh
+++ scriptmodules/emulators/openbor-extra.sh
@@ -67,5 +67,5 @@ function configure_openbor-extra() {
     ln -snf "$romdir/ports/$md_id" "$md_inst/Paks"
     ln -snf "/dev/shm" "$md_inst/Logs"
     addEmulator 0 "$md_id" "openbor-extra" "$md_inst/OpenBOR %ROM%"
-    addSystem "openbor"
+    addSystem "openbor" "OpenBOR" ".pak .bor"
 }
